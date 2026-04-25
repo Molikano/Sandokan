fb439c667a3e9e2e11090a6a7e5dddbef1c561b5
anastassow <127148449+anastassow@users.noreply.github.com>
2026-04-26 00:21:23 +0200

    updated deps

--- include/sandokan.h
+++ include/sandokan.h
@@ -2,9 +2,24 @@
 
 // Core Sandokan Library
 #include "sandokan/allocator.h"
-#include "sandokan/dataset.h"
-#include "sandokan/layer.h"
-#include "sandokan/network.h"
-#include "sandokan/ops.h"
-#include "sandokan/train.h"
 #include "sandokan/workspace.h"
+#include "sandokan/ops.h"
+
+// Layer system
+#include "sandokan/layer.h"
+#include "sandokan/layers.h"
+#include "sandokan/module.h"
+
+// Network & training
+#include "sandokan/network.h"
+#include "sandokan/loss.h"
+#include "sandokan/optim.h"
+#include "sandokan/train.h"
+
+// Data
+#include "sandokan/dataset.h"
+#include "sandokan/tabular.h"
+
+// Pipeline
+#include "sandokan/inference.h"
+#include "sandokan/io.h"
